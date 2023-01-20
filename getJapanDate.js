77ada99a1a197be3956a7bb5ceac22893262ae9d
pakkunat <pakkun_kh@fol.hi-ho.ne.jp>
2023-01-20 18:54:21 +0900

    the first commit

---
+++ getJapanDate.js
@@ -0,0 +1,19 @@
+function getJapanDate(date) {
+  const reiwaYear = 2019;
+  const reiwaMonth = 5;
+  const reiwaDay = 1;
+
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+
+  let y = year - reiwaYear + 1;
+  var japanYear = "";
+  if (y == 1) {
+    japanYear = "元";
+  } else {
+    japanYear = y.toString();
+  }
+
+  return Utilities.formatString("令和%s年%s月%s日", japanYear, month, day);
+}
